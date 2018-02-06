e594efe5ffa83f7d74d25733850457b7b8d78f18
Matt Adams <matt@offhourscoding.com>
2018-02-05 23:02:09 -0500

    setup angular frontend

---
+++ angular-src/src/app/modules/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material';
+
+@NgModule({
+  imports: [ MatButtonModule ],
+  exports: [ MatButtonModule ],
+})
+
+export class MaterialModule {  }
